4677e6377c811dc61a04f758efd1af4cc1c4264f
Sami Aitsaadi <sa02874@surrey.ac.uk>
2024-05-24 00:24:52 +0100

    BEFORE DOCKER

--- nlp_classifier/models.py
+++ nlp_classifier/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Prediction(models.Model):
+    input_text = models.TextField()
+    output_text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    total_time = models.FloatField(default=0.0)  
+
+    def __str__(self):
+        return f"Prediction {self.id} at {self.created_at}"
