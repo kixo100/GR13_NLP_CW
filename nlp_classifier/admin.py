d4f3078363ef1c3562dadd0be4fb8e0d85d1da01
Sami Aitsaadi <sa02874@surrey.ac.uk>
2024-05-23 22:16:21 +0100

    Working final code.

--- nlp_classifier/admin.py
+++ nlp_classifier/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Prediction
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,3 +7,7 @@ urlpatterns = [
     path('', include('nlp_classifier.urls')),
 ]
 
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('input_text', 'created_at')
+    search_fields = ('input_text', 'output_text')
--- nlp_classifier/views.py
+++ nlp_classifier/views.py
@@ -1,43 +1,74 @@
+import logging
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
 from transformers import RobertaForTokenClassification, AutoTokenizer, pipeline
-from torch import load, device
+from .models import Prediction
+from django.utils import timezone
+import time
 
+# Load the tokenizer and model
 original_model_name = "FacebookAI/roberta-base"
-tokenizer = AutoTokenizer.from_pretrained(original_model_name, add_prefix_space=True,)
-
+tokenizer = AutoTokenizer.from_pretrained(original_model_name, add_prefix_space=True)
 model_name = "theglassofwater/NLP_GROUP13_ROBERTA_BASE"
 model = RobertaForTokenClassification.from_pretrained(model_name)
-print(model)
-token_classifier = pipeline(
-"token-classification", model=model, tokenizer=tokenizer
-)
+token_classifier = pipeline("token-classification", model=model, tokenizer=tokenizer)
+
+# logging configuration 
+logging.basicConfig(filename='predictions.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def predict_(text):
+    start_time = time.time()
     output = token_classifier(text)
+    end_time = time.time()
     indexes = [[i["start"], i["end"]] for i in output]
     ner_tags = [i["entity"] for i in output]
-    highlighting = [""]*len(text)
-    for tag, range in zip(ner_tags, indexes):
-        start, end = range
-        length = end-start
-        new_tags = [tag]*length
+    highlighting = [""] * len(text)
+    for tag, range_ in zip(ner_tags, indexes):
+        start, end = range_
+        length = end - start
+        new_tags = [tag] * length
         highlighting[start:end] = new_tags
-    return ner_tags #, indexes, highlighting
-
-# def classify_sentence(sentence):
-#     tokens = sentence.split()
-#     tags = ['B-O'] * len(tokens)  # Dummy tags for demonstration
-#     return list(zip(tokens, tags))
+    generate_time = end_time - start_time
+    return ner_tags, indexes, highlighting, generate_time
 
 @csrf_exempt
 def predict(request):
-    prediction = None
+    total_start_time = time.time()
+
+    sentence = ""
+    highlighted_sentence = ""
+    tag_colors = {
+        "B-O": "gray",
+        "B-AC": "red",
+        "B-LF": "green",
+        "I-LF": "darkgreen",
+        
+    }
+
     if request.method == 'POST':
         data = request.POST.get('sentence')
         if data:
-            prediction = predict_(data)
+            sentence = data
+
+            ner_tags, indexes, highlight, generate_time = predict_(data)  
+            
+            
+            for char, tag in zip(sentence, highlight):
+                color_class = tag_colors.get(tag, "black")
+                highlighted_sentence += f'<span style="color: {color_class}">{char}</span>'
+            
+            # total time taken
+            total_time = time.time() - total_start_time
+            
+            # Save prediction to database with ner_tags
+            output_tags = " ".join(ner_tags)
+            Prediction.objects.create(
+                input_text=sentence,
+                output_text=output_tags,
+                total_time=total_time 
+            )
+            
         
-    return render(request, 'index.html', {'prediction': prediction})
+            logging.info(f"Input: {sentence}, Output: {output_tags}, Created At: {timezone.now()}, Total Time: {total_time}")
+
+    return render(request, 'index.html', {'sentence': sentence, 'prediction': highlighted_sentence, 'tag_colors': tag_colors})
